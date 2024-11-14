c7ea2ff4957a87bff6d8603ade37608e201f1bb4
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-11-13 21:42:28 -0500

    fix: javax dependencies issue fixed

--- src/main/java/com/agrotech/controller/DashboardController.java
+++ src/main/java/com/agrotech/controller/DashboardController.java
@@ -1,4 +1,23 @@
 package com.agrotech.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DashboardController {
+    @FXML private BorderPane mainContainer;
+    @FXML private VBox topPanel;
+    @FXML private Label dateLabel;
+    @FXML private Label cropTypeLabel;
+    @FXML private Label systemStatusLabel;
+
+    @FXML
+    public void initialize() {
+        setupTopPanel();
+        setupMainPanel();
+        setupBottomPanel();
+    }
 }
