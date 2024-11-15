c2fa452a088d3943386bd2b854f455029c128f29
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-11-15 00:50:50 -0500

    feat: new methods added to view

--- src/main/java/com/agrotech/view/WelcomeView.java
+++ src/main/java/com/agrotech/view/WelcomeView.java
@@ -1,4 +1,24 @@
 package com.agrotech.view;
 
-public class WelcomeView {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class WelcomeView extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/WelcomeView.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
