ed9b81400e57860f917ac7ac348de2ff6dc66334
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2024-11-18 20:49:46 -0500

    bug fixed

--- src/main/java/com/agrotech/Main.java
+++ src/main/java/com/agrotech/Main.java
@@ -82,7 +82,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        System.setProperty("javafx.preloader", "com.agrotech.preloader.AppPreloader");
         launch(args);
     }
 }
